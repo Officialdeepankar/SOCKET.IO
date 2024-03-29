38a85d65dce2d5d37e5ec870d573e3adc35763da
Deepankar singh <singhdeepankarpost@gmail.com>
2024-03-29 19:51:28 +0530

    "hello-message"

--- index.js
+++ index.js
@@ -12,16 +12,11 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket) => {
-    console.log('a user connected');
-    socket.on('disconnect', () => {
-      console.log('user disconnected');
-    });
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
   });
+});
 
 server.listen(3000, () => {
   console.log('server running at http://localhost:3000');
 });
-
-
-
-
